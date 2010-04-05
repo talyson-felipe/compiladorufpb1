c09decd7d70ddd8d70292b237f71dadbdd1e5be6
clodbrasilino <clodbrasilino@4f02ddce-fc8a-c77d-be43-81c44d7564fc>
2010-04-05 15:15:21 +0000

    Modelo do analisador léxico - Erros e Tokens

---
+++ CompiladorUFPB1/src/modelo/tipos/TipoToken.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo.tipos;
+
+/**
+ *
+ * @author clodbrasilino
+ */
+public enum TipoToken {
+    IDENTIFICADOR,
+    NUMERO_INTEIRO,
+    NUMERO_REAL,
+    DELIMITADOR,
+    COMANDO_ATRIBUICAO,
+    COMANDO_RELACIONAL,
+    OPERADOR_RELACIONAL,
+    OPERADOR_ADITIVO,
+    OPERADOR_MULTIPLICATIVO,
+    INVALIDO
+}
