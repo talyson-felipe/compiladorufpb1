6657c9bb35b59dcfaca1b2fd0f6c0c5596d58e6e
clodbrasilino <clodbrasilino@4f02ddce-fc8a-c77d-be43-81c44d7564fc>
2010-04-11 14:36:55 +0000

    Analisador Lexico quase completo - Ainda falta alguns regex a serem adicionados e testes, mas montagem de objetos do modelo completa.
    Pequeno método de acesso à interface adicionado, referenciando o analisador léxico.

--- trunk/CompiladorUFPB1/src/modelo/tipos/TipoToken.java
+++ trunk/CompiladorUFPB1/src/modelo/tipos/TipoToken.java
@@ -10,14 +10,15 @@ package modelo.tipos;
  * @author clodbrasilino
  */
 public enum TipoToken {
+    PALAVRA_RESERVADA,
     IDENTIFICADOR,
     NUMERO_INTEIRO,
     NUMERO_REAL,
     DELIMITADOR,
     COMANDO_ATRIBUICAO,
-    COMANDO_RELACIONAL,
     OPERADOR_RELACIONAL,
     OPERADOR_ADITIVO,
     OPERADOR_MULTIPLICATIVO,
+    COMENTARIO,
     INVALIDO
 }
