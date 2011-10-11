407f6d5cbea2b9f853335846b2c9a2dbd31876f6
clodbrasilino <clodbrasilino@4f02ddce-fc8a-c77d-be43-81c44d7564fc>
2011-10-11 13:36:37 +0000

    Exceptions que faltavam.

---
+++ trunk/CompiladorUFPB1/src/analisador/exceptions/OpenProjectException.java
@@ -0,0 +1,11 @@
+package analisador.exceptions;
+
+/**
+ *
+ * @author Erisvaldo
+ */
+public class OpenProjectException extends Exception {
+    public OpenProjectException(String message) {
+        super(message);
+    }
+}
